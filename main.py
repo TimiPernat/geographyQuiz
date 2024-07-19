77e73a58f39329384c151e28eebd0fcf1a3067cc
TimiPernat <timotej.pernat@gmail.com>
2024-07-19 10:28:41 +0200

    Funkcionalnost
    Dodal sem logiko shranjevanja glavnih mest katere je uporabnit že ugotovil in dodal sem da se država ne more ponovit ena za drugo

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ def reading_data():
 
 def play_quiz():
 
+    guessed_countries = []
     streak = 0
     random_country = random.choice(list(reading_data().items()))
 
     while True:
+
+        if random_country in guessed_countries:
+            random_country = random.choice(list(reading_data().items()))
+
         answer = input(f"Glavno mesto {random_country[0]} (napiši Hint, za namig): ").capitalize()
 
         if answer == "Hint":
@@ -21,9 +26,10 @@ def play_quiz():
         elif answer == random_country[1]:
             print("Odgovor je pravilen!")
             streak += 1
+            guessed_countries.append(random_country)
             random_country = random.choice(list(reading_data().items()))
         else:
-            print("Več sreče prihodnjič.")
+            print(f"Več sreče prihodnjič. Pravilen odgovor je {random_country[1]}")
             print(f"Ugotovil si {streak} glavnih mest.")
             break
 
